7b9de675f39abc6623fe090123372c0fa32bc20c
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-10-18 21:56:08 +0100

    Correzione, esercizio completo

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
 */
 
 $paragrafo ="In una terra lontana, dietro le montagne Parole, lontani dalle terre di Vocalia e Consonantia, vivono i testi di bisogno. Vivono isolati nella cittadina di Lettere, sulle coste del Semantico bisogno, un immenso oceano linguistico. Un piccolo ruscello chiamato Devoto Oli attraversa quei luoghi, rifornendoli di tutte le regolalie di cui hanno bisogno.";
-$censura = $_GET["censura"]
-
+$censura = $_GET["censura"];
+$censurata = str_replace($censura,"***",$paragrafo);
 ?>
 
 <!DOCTYPE html>
@@ -24,7 +24,9 @@ $censura = $_GET["censura"]
 <body>
     <p><h1>Il paragrafo è:</h1><?php echo $paragrafo ?></p>
     <p>La lunghezza del paragrafo è di <?php echo strlen($paragrafo) ?> caratteri</p>
-    <h1>Inserire la parola da sovrascrivere alla parola censurata(bisogno) tramite get</h1>
-    <p><?php echo str_replace("bisogno", $censura, $paragrafo) ?></p>
+    <h1>Inserire la parola da sovrascrivere alla parola censurata tramite get</h1>
+    <h2>NUOVO PARAGRAFO</h2>
+    <p><?php echo $censurata ?></p>
+    <p>La lunghezza del nuovo paragrafo è di <?php echo strlen($censurata) ?> caratteri</p>
 </body>
 </html>
